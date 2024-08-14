2886a0542f25619109b835780d72e7834834fc31
Gabriel Carvalho <62999503+gabriielk0@users.noreply.github.com>
2024-08-14 12:55:19 -0300

    Inserindo Script que calcula a Média

---
+++ media_nota.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Insira a primeira nota: "))
+nota2 = float(input("Insira a segunda nota: "))
+nota3 = float(input("Insira a terceira nota: "))
+
+total = nota1 + nota2 + nota3
+
+media = total/3
+
+print("Média das notas ", nota1, nota2, nota3, "é ", media)
