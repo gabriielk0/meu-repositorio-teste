fd5cf3d1fb72ae60765f59029384c3b7e453168e
Gabriel Carvalho <62999503+gabriielk0@users.noreply.github.com>
2024-08-14 12:46:06 -0300

    Adicionando Operação matematica

---
+++ operacao_math.py
@@ -0,0 +1,6 @@
+num1 = float(input("Insira o primeiro número: "))
+num2 = float(input("Insira o segundo número: "))
+
+resultado = num1 * num2
+
+print("A multiplicação de " , num1 , "e" , num2 , "é igual a" , resultado)
